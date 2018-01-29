70d5d412181c010abcd72230b767b499e4f5046e
tang.biao <tang.biao@chinaebi.com>
2018-01-29 13:22:22 +0800

    test

---
+++ hello/src/main/java/com/tang/hello/action/HelloController.java
@@ -0,0 +1,14 @@
+package com.tang.hello.action;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/user/{name}")
+	public String hello(@PathVariable String name){
+		return "Hello, "+ name;
+	}
+}
